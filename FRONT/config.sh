5b8395c0726bbea601b6aa60a8cff478b639fb4b
johanOA <johana.ospinao@uqvirtual.edu.co>
2024-10-04 21:59:32 -0500

    Pruebas

--- FRONT/config.sh
+++ FRONT/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Eliminar el gatewat predeterminado
-route del default
+#route del default
 
 # Agregar el gateway del firewall predeterminada
-ip route add default via 10.0.10.10
+#ip route add default via 10.0.10.10
