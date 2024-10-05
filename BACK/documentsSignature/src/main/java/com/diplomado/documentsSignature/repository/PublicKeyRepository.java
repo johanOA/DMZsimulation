5869749d04785f477280f6c8d54dcdb65057e6f6
johanOA <johana.ospinao@uqvirtual.edu.co>
2024-10-05 11:34:25 -0500

    Se elimino la carpeta del BACK

--- BACK/documentsSignature/src/main/java/com/diplomado/documentsSignature/repository/PublicKeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.diplomado.documentsSignature.repository;
-
-import com.diplomado.documentsSignature.model.PublicKey;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublicKeyRepository extends JpaRepository<PublicKey,Long> {
-}
